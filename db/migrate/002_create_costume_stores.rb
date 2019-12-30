b4e4bf548f2e1a56fbb9f47d7cab57e492a1d664
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-30 21:45:51 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |x|
+    x.string :name
+    x.string :location
+    x.integer :costume_inventory
+    x.integer :num_of_employees
+    x.boolean :still_in_business
+    x.datetime :opening_time
+    x.datetime :closing_time
+   end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |x|
+      x.string :name
+      x.string :location
+      x.string :theme
+      x.float :price
+      x.boolean :family_friendly
+      x.datetime :opening_date
+      x.datetime :closing_date
+      x.text :description
+    end
+  end
+end
