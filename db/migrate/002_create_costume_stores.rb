b25594869e854fc75dd0b90101571ddce60797dd
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-30 21:49:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,13 @@
 class CreateCostumes < ActiveRecord::Migration[4.2]
   def change
-    create_table :costume_stores do |x|
-    x.string :name
-    x.string :location
-    x.integer :costume_inventory
-    x.integer :num_of_employees
-    x.boolean :still_in_business
-    x.datetime :opening_time
-    x.datetime :closing_time
+    create_table :costume_stores do |t|
+    t.string :name
+    t.string :location
+    t.integer :costume_inventory
+    t.integer :num_of_employees
+    t.boolean :still_in_business
+    t.datetime :opening_time
+    t.datetime :closing_time
    end
   end
 end
