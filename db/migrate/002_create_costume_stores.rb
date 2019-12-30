6f11b020d648b013b52b911e5d2c0cd73de4f7d9
Taylor Kraft <taylorkraft16@gmail.com>
2019-12-30 21:50:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,4 @@
-class CreateCostumeStore < ActiveRecord::Migration[4.2]
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
     t.string :name
